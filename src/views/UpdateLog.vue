f5d585d9faf9f200ef4ef7bdc742535ab7ab0d85
NicholasLD <878639947@qq.com>
2023-06-07 03:19:56 +0800

    更新日志页面

---
+++ src/views/UpdateLog.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+
+<template>
+  <el-timeline>
+    <el-timeline-item timestamp="2023/6/7" placement="top">
+      <el-card>
+        <h4>更新内容：</h4>
+        <ul>
+          <li>添加了更新日志页面 🤔</li>
+          <li>优化了菜单，将表情包制作添加进子菜单。</li>
+          <li>修改了制作页面表情包展示，手机现在可以长按保存制作的表情。</li>
+        </ul>
+        <p>本次更新代码没有及时推送至GitHub和官网，因为写到这已经凌晨三点了，白天再推送吧。</p>
+        <img src="https://img.nicholasld.cn/i/2023/06/07/647f848f2a265.png" alt="">
+      </el-card>
+    </el-timeline-item>
+    <el-timeline-item center timestamp="2023/6/4" placement="top">
+      初版完工，项目上线
+    </el-timeline-item>
+    <el-timeline-item timestamp="2023/6/3" placement="top">
+      项目开工，仓库创建
+    </el-timeline-item>
+  </el-timeline>
+</template>
+
+<style scoped>
+  p {
+    font-size: 17px;
+    color: #999;
+  }
+
+  li {
+    font-size: 16px;
+    color: #999;
+    line-height: 1.3;
+  }
+</style>
